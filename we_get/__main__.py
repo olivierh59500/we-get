3f7a4f5b00a44115271294e85b90c8b588a6e8d7
Levi Sabah <0xl3vi@gmail.com>
2017-03-16 20:40:41 +0000

    we_get/__main__.py: refactor call to main()

--- we_get/__main__.py
+++ we_get/__main__.py
@@ -3,4 +3,6 @@ Copyright (c) 2017 we-get developers (https://github.com/0xl3vi/we-get/)
 See the file 'LICENSE' for copying permission
 """
 from we_get.__init__ import main 
-main()
+
+if __name__ == '__main__':
+    main()
